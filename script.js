d974b3c0caba216ac64a93e49076d031215bd0b5
mariarosadelatex <mariarosadelatex@gmail.com>
2019-10-09 18:28:31 -0600

    added time

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-var submitButton = document.querySelector()
+//setting my variables for the planner
+
+var submitButton = document.querySelectorAll(".button");
+var textSubmit = document.querySelectorAll(".holder");
+var timeSelect = document.querySelectorAll(".time");
+var date = document.querySelector("#currentDay");
+
+var m = moment().format("dddd MMMM Do");
+
+console.log(m.toString("dddd MMMM Do"));
+date.innerHTML = m;
+
+console.log(submitButton);
+console.log(textSubmit);
+
+//after each click the variables will be saved to local storage
+
+
+$(".button").on("click", function() { 
+
+console.log(submitButton);
+
+
+var timing = timeSelect.textContent;
+var userText = textSubmit.value;
+
+localStorage.setItem("tiempo", timing);
+console.log(timing);
+localStorage.setItem("input", userText);
+console.log(userText);
+event.preventDefault();
+} 
+
+)
