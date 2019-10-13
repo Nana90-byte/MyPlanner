e6213cf79ebc0f733ff5db74f006a20ca149460e
mariarosadelatex <mariarosadelatex@gmail.com>
2019-10-12 22:47:13 -0600

    added pseudo code

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
+
+//function that displays the date in the console and browser
 function dator() {
     var date = document.querySelector("#currentDay");
     var m = moment().format("dddd MMMM Do");
     console.log(m.toString("dddd MMMM Do"));
     date.innerHTML = m;
 }
+//Updates and runs the function every minute on the date display
 setInterval(dator, 1000 * 60 );
+//Calls the function
 dator();
 
 
-// var t = moment().format("H");
-// console.log(t.toString("H"));
 
-// get out data from local storage
+
+// get out data from local storage and save it in the browser
 for (var i = 9; i < 18; i++) {
     console.log(
         i,
@@ -19,7 +22,7 @@ for (var i = 9; i < 18; i++) {
     )
     $("#textarea" + i).val(localStorage.getItem(i))
 }
-
+//Using an array checks the current hour of the day and compares it to the  hour of the text area, and with a conditional statement it will change color depending on the hour
 function colorBlocks() {
     var t = moment().format("H");
     console.log(t.toString("H"));
@@ -39,7 +42,7 @@ function colorBlocks() {
         }
     }
 }
-
+//Updates and runs the function every minute on the time blocks
 setInterval(colorBlocks, 1000 * 60);
 colorBlocks();
 
@@ -54,7 +57,7 @@ $(".saveBtn").on("click", function () {
 })
 
 
-r
+
 
 
 
